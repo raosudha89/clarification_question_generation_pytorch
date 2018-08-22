bc3595c1ecf045097aa3334e250bee04df9d839e
Sudha Rao <raosudha@umiacs.umd.edu>
2018-08-21 21:00:43 -0400

    code modularization

--- src/constants.py
+++ src/constants.py
@@ -2,12 +2,10 @@ USE_CUDA = True
 
 # Configure models
 attn_model = 'dot'
-#hidden_size = 500
 hidden_size = 100
-n_layers = 2
-dropout = 0.1
-#batch_size = 100
-batch_size = 256
+n_layers = 1
+dropout = 0.5
+batch_size = 64
 word_emb_size = 200
 
 # Configure training/optimization
@@ -15,18 +13,16 @@ clip = 50.0
 teacher_forcing_ratio = 0.5
 learning_rate = 0.0001
 decoder_learning_ratio = 5.0
-n_epochs = 100
+n_epochs = 20
 epoch = 0.
-plot_every = 20
-print_every = 10
-evaluate_every = 10
 
-MIN_COUNT = 2
-PAD_token = 0
-SOS_token = 1
-EOS_token = 2
+PAD_token = '<PAD>'
+SOS_token = '<SOS>'
+EOP_token = '<EOP>'
+EOS_token = '<EOS>'
 MAX_POST_LEN=300
 MAX_QUES_LEN=50
+MAX_ANS_LEN=50
 MIN_TFIDF=30
 clip = 50.0
 
--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import argparse
+import os
+import cPickle as p
 import string
-import random
 import time
 import datetime
 import math
@@ -9,7 +10,6 @@ import sys
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
-from torch import optim
 import torch.nn.functional as F
 from torch.nn.utils.rnn import pad_packed_sequence, pack_padded_sequence#, masked_cross_entropy
 
@@ -17,98 +17,75 @@ import numpy as np
 
 from seq2seq.read_data import *
 from seq2seq.prepare_data import *
-from seq2seq.encoderRNN import *
-from seq2seq.encoderAvgEmb import *
-from seq2seq.attnDecoderRNN import *
 from seq2seq.masked_cross_entropy import *
-from seq2seq.helper import *
-from seq2seq.attn import *
-from seq2seq.train import *
-from seq2seq.evaluate import *
+from seq2seq.main import * 
+from utility.main import *
+from RL_constants import *
 
-from constants import *
-
-def load_pretrained_emb(word_vec_fname, p_data):
-	pretrained_emb = np.random.rand(p_data.n_words, word_emb_size)
-	word_vec_file = open(word_vec_fname, 'r')
-	for line in word_vec_file.readlines():
-		vals = line.rstrip().split(' ')
-		word = vals[0]
-		if word in p_data.word2index:
-			pretrained_emb[p_data.word2index[word]] = map(float, vals[1:])
-	return pretrained_emb
+def update_embs(word2index, word_embeddings):
+	word_embeddings[word2index[PAD_token]] = nn.init.normal_(torch.empty(1, len(word_embeddings[0])))
+	word_embeddings[word2index[SOS_token]] = nn.init.normal_(torch.empty(1, len(word_embeddings[0])))
+	word_embeddings[word2index[EOP_token]] = nn.init.normal_(torch.empty(1, len(word_embeddings[0])))
+	word_embeddings[word2index[EOS_token]] = nn.init.normal_(torch.empty(1, len(word_embeddings[0])))
+	return word_embeddings
 
 def main(args):
+	word_embeddings = p.load(open(args.word_embeddings, 'rb'))
+	word_embeddings = np.array(word_embeddings)
+	word2index = p.load(open(args.vocab, 'rb'))
+	#word_embeddings = update_embs(word2index, word_embeddings) --> updating embs gives poor utility results (0.5 acc)
+	index2word = reverse_dict(word2index)
 
-	p_data, q_data, train_data, test_data = prepare_data(args.train_src, args.train_tgt, \
-															args.test_src, args.test_tgt)
+	train_data = read_data(args.train_context, args.train_question, args.train_answer, split='train')
+	test_data = read_data(args.tune_context, args.tune_question, args.tune_answer, split='test')
+	#test_data = read_data(args.test_context, args.test_question, args.test_answer, split='test')
 
-	#pretrained_emb = load_pretrained_emb(args.word_vec_fname, p_data)
-
-	# Initialize models
-	#encoder = EncoderAvgEmb(pretrained_emb)
-	encoder = EncoderRNN(p_data.n_words, hidden_size, n_layers, dropout=dropout)
-	decoder = AttnDecoderRNN(attn_model, hidden_size, q_data.n_words, n_layers)
-
-	# Initialize optimizers and criterion
-	encoder_optimizer = optim.Adam(encoder.parameters(), lr=learning_rate)
-	decoder_optimizer = optim.Adam(decoder.parameters(), lr=learning_rate * decoder_learning_ratio)
-	criterion = nn.CrossEntropyLoss()
-
-	# Move models to GPU
-	if USE_CUDA:
-		encoder.cuda()
-		decoder.cuda()
-
-	# Keep track of time elapsed and running averages
-	start = time.time()
-	print_loss_total = 0 # Reset every print_every
-	epoch = 0.0
-	
 	print 'No. of train_data %d' % len(train_data)
 	print 'No. of test_data %d' % len(test_data)
 
-	input_seqs, target_seqs = preprocess_data(p_data, q_data, train_data, shuffle=True)
+	post_seqs, post_lens, ques_seqs, ques_lens, \
+            post_ques_seqs, post_ques_lens, ans_seqs, ans_lens = preprocess_data(train_data, word2index)
 
-	n_batches = len(train_data) / batch_size
-	while epoch < n_epochs:
-		epoch += 1
-		if epoch > n_epochs - 10:
-			out_file = open(args.test_pred+'.epoch%d' % int(epoch), 'w')	
-		else:
-			out_file = None
-		for input_seqs_batch, target_seqs_batch in \
-				iterate_minibatches(input_seqs, target_seqs, batch_size):	
+	q_train_data = post_seqs, post_lens, ques_seqs, ques_lens
+	a_train_data = post_ques_seqs, post_ques_lens, ans_seqs, ans_lens
+	u_train_data = post_seqs, post_lens, ques_seqs, ques_lens, ans_seqs, ans_lens
 
-			input_batch, input_lens, target_batch, target_lens = \
-				add_padding(input_seqs_batch, target_seqs_batch, USE_CUDA)
+	post_seqs, post_lens, ques_seqs, ques_lens, \
+            post_ques_seqs, post_ques_lens, ans_seqs, ans_lens = preprocess_data(test_data, word2index)
 
-			start_time = time.time()
-			# Run the train function
-			loss, ec, dc = train(
-				input_batch, input_lens, 
-				target_batch, target_lens,
-				encoder, decoder,
-				encoder_optimizer, decoder_optimizer, criterion, USE_CUDA
-			)
-	
-			# Keep track of loss
-			print_loss_total += loss
-		
-		print_loss_avg = print_loss_total / n_batches
-		print_loss_total = 0
-		print_summary = '%s %d %.4f' % (time_since(start, epoch / n_epochs), epoch, print_loss_avg)
-		print(print_summary)
-		evaluate_testset(p_data, q_data, encoder, decoder, test_data, batch_size, out_file)
-	
+	q_test_data = post_seqs, post_lens, ques_seqs, ques_lens
+	a_test_data = post_ques_seqs, post_ques_lens, ans_seqs, ans_lens
+	u_test_data = post_seqs, post_lens, ques_seqs, ques_lens, ans_seqs, ans_lens
+
+	#run_seq2seq(q_train_data, q_test_data, word2index, index2word, \
+	#		word_embeddings, args.test_pred_question, args.q_encoder_params, args.q_decoder_params)
+
+	#run_seq2seq(a_train_data, a_test_data, word2index, index2word, \
+	#		word_embeddings, args.test_pred_question, args.a_encoder_params, args.a_decoder_params)
+
+	run_utility(u_train_data, u_test_data, word_embeddings, args.utility_params)
+
 if __name__ == "__main__":
 	argparser = argparse.ArgumentParser(sys.argv[0])
-	argparser.add_argument("--train_src", type = str)
-	argparser.add_argument("--train_tgt", type = str)
-	argparser.add_argument("--test_src", type = str)
-	argparser.add_argument("--test_tgt", type = str)
-	argparser.add_argument("--test_pred", type = str)
-	#argparser.add_argument("--word_vec_fname", type = str)
+	argparser.add_argument("--train_context", type = str)
+	argparser.add_argument("--train_question", type = str)
+	argparser.add_argument("--train_answer", type = str)
+	argparser.add_argument("--tune_context", type = str)
+	argparser.add_argument("--tune_question", type = str)
+	argparser.add_argument("--tune_answer", type = str)
+	argparser.add_argument("--test_context", type = str)
+	argparser.add_argument("--test_question", type = str)
+	argparser.add_argument("--test_answer", type = str)
+	argparser.add_argument("--test_pred_question", type = str)
+	argparser.add_argument("--test_pred_answer", type = str)
+	argparser.add_argument("--q_encoder_params", type = str)
+	argparser.add_argument("--q_decoder_params", type = str)
+	argparser.add_argument("--a_encoder_params", type = str)
+	argparser.add_argument("--a_decoder_params", type = str)
+	argparser.add_argument("--utility_params", type = str)
+	argparser.add_argument("--vocab", type = str)
+	argparser.add_argument("--word_embeddings", type = str)
+	argparser.add_argument("--cuda", type = bool)
 	args = argparser.parse_args()
 	print args
 	print ""
