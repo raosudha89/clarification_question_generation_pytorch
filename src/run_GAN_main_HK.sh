a2721b6a59529b7afa3c64b276c3e319e88b9d17
Sudha Rao <raosudha89@gmail.com>
2020-08-14 16:15:55 -0700

    Update run_GAN_main_HK.sh

--- src/run_GAN_main_HK.sh
+++ src/run_GAN_main_HK.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 
-#SBATCH --job-name=GAN_selfcritic_pred_ans_3perid_util_dis
-#SBATCH --output=GAN_selfcritic_pred_ans_3perid_util_dis
-#SBATCH --qos=gpu-long
-#SBATCH --partition=gpu
-#SBATCH --gres=gpu
-#SBATCH --time=48:00:00
-#SBATCH --mem=128g
-
 SITENAME=Home_and_Kitchen
 
-CQ_DATA_DIR=/fs/clip-amr/clarification_question_generation_pytorch/$SITENAME
-PARAMS_DIR=/fs/clip-scratch/raosudha/clarification_question_generation/$SITENAME
-SCRIPT_DIR=/fs/clip-amr/clarification_question_generation_pytorch/src
-EMB_DIR=/fs/clip-amr/clarification_question_generation_pytorch/embeddings/$SITENAME
+CQ_DATA_DIR=clarification_question_generation_pytorch/$SITENAME
+SCRIPT_DIR=clarification_question_generation_pytorch/src
+EMB_DIR=clarification_question_generation_pytorch/embeddings/$SITENAME
 
-#module load cuda/9.1.85
+PARAMS_DIR=$CQ_DATA_DIR
 
 python $SCRIPT_DIR/GAN_main.py	--train_context $CQ_DATA_DIR/train_context.txt \
 									--train_ques $CQ_DATA_DIR/train_ques.txt \
